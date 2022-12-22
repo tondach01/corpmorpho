e0a8ac81450f6dad6bc041f87a03366732006f7f
xmetelk1 <xmetelk1@mail.muni.cz>
2022-12-22 13:28:12 +0100

    paradigm frequencies working

--- db_stats.py
+++ db_stats.py
@@ -1,24 +1,27 @@
 import morph_database as md
 from typing import Dict
+from os import sep
 
 
 def paradigm_frequencies(corpus: str, morph_db: md.MorphDatabase) -> Dict[str, int]:
+    """Computes frequencies of words belonging to single paradigm in corpus"""
     freqs = dict()
     corp = open(corpus, encoding="utf-8")
     for line in corp:
         if len(line.split("\t")) != 3:
             continue
         lemma = line.split("\t")[1]
-        if lemma not in morph_db.paradigms.keys():
+        if lemma not in morph_db.vocab.keys():
             continue
-        paradigm = morph_db.paradigms[lemma]
+        paradigm = morph_db.vocab[lemma]
         freqs[paradigm] = freqs.get(paradigm, 0) + 1
     corp.close()
     return freqs
 
 
 def main():
-    pass
+    desam = f"C:{sep}Users{sep}ondra{sep}Desktop{sep}MUNI{sep}PB106{sep}data{sep}desam_model{sep}desam"
+    paradigm_frequencies(desam, md.MorphDatabase("current.dic", "current.par"))
 
 
 if __name__ == "__main__":
