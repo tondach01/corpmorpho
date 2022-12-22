826f053eeed5a5bd3f113a0411513635cb3b4097
xmetelk1 <xmetelk1@mail.muni.cz>
2022-12-22 16:01:25 +0100

    documentation

--- morph_database.py
+++ morph_database.py
@@ -7,22 +7,25 @@ FORMS = Dict[str, Union[str, List[str]]]
 PARADIGMS_TAIL_GROUPS = Dict[str, FORMS]
 MORPH_DATABASE = Dict[str, Dict[str, Dict[str, int]]]
 VOCABULARY = Dict[str, str]
-SUFFICES_DATABASE = Dict[str, Dict[str, int]]
 
 
 class MorphDatabase:
+    """This class represents morphological database obtained from dictionary and paradigm files. Holds
+    attributes vocab (dictionary lemma:paradigm) and paradigms (paradigm:suffixes and tags)."""
     def __init__(self, dic_file: str, par_file: str):
         self.vocab = vocabulary(dic_file)
         self.paradigms = translate_morph_db(par_file)
         self.paradigm_roots()
 
     def full_database(self) -> MORPH_DATABASE:
+        """Returns dictionary of words and all their forms and frequencies"""
         database = dict()
         for lemma, paradigm in self.vocab.items():
             self.add_lemma_forms(lemma, paradigm, database)
         return database
 
     def add_lemma_forms(self, lemma: str, paradigm: str, database: MORPH_DATABASE) -> None:
+        """Constructs all forms for given lemma, assigns them frequency 0 and writes them to database"""
         database[lemma] = dict()
         forms = self.all_forms_with_paradigm(lemma, paradigm)
         for form in forms.keys():
@@ -31,22 +34,24 @@ class MorphDatabase:
                 database[lemma][form][tag] = 0
 
     def all_forms(self, lemma: str) -> FORMS:
+        """Constructs all forms for given lemma"""
         paradigm = self.find_paradigm(lemma) if lemma not in self.paradigms.keys() else lemma
         if paradigm:
             return self.all_forms_with_paradigm(lemma, paradigm)
         return dict()
 
     def all_forms_with_paradigm(self, lemma: str, paradigm: str) -> FORMS:
+        """Constructs all forms for given lemma when its paradigm is known"""
         forms = dict()
         root = self.word_root(lemma, paradigm)
         for form, tags in self.paradigms[paradigm].items():
             if form == "<suffix>":
                 continue
-            forms[root+form] = forms.get(root+form, [])
-            forms[root+form].extend(tags)
+            forms[root+form] = forms.get(root+form, []) + tags
         return forms
 
     def word_root(self, lemma: str, paradigm: str) -> str:
+        """Finds the morphological root (resp. prefixes+root) for given lemma"""
         if lemma.endswith("ný") or (lemma.endswith("cí") and not lemma.endswith("ící")) or lemma.endswith("tý") \
                 or lemma.endswith("ový") or lemma.endswith("lý") \
                 or (lemma.endswith("ní") and not lemma.endswith("ání") and not lemma.endswith("ení")):
@@ -57,6 +62,7 @@ class MorphDatabase:
         return lemma[:-common_suffix]
 
     def word_form(self, lemma: str, tag_part: str) -> Set[str]:
+        """Returns all forms of given lemma which tags are matching tag_part"""
         found_forms = set()
         for form in self.all_forms(lemma).keys():
             for tag in form:
@@ -65,6 +71,7 @@ class MorphDatabase:
         return found_forms
 
     def lemma_tag(self, form: str) -> List[Dict[str, str]]:
+        """Finds all possible lemma-tag pairs for gives word form"""
         found_lt = []
         for lemma, paradigm in self.vocab.items():
             root = self.word_root(lemma, paradigm)
@@ -73,6 +80,7 @@ class MorphDatabase:
         return found_lt
 
     def add_similar_forms(self, root: str, paradigm: str, form: str, found_lt: List[Dict[str, str]]) -> None:
+        """Searches through single paradigm and appends all lemma-tag pairs matching form"""
         for paradigm_form, tags in self.paradigms[paradigm].items():
             if paradigm_form == "<suffix>":
                 continue
@@ -80,9 +88,11 @@ class MorphDatabase:
                 found_lt += [dict(lemma=(root + self.paradigms[paradigm]["<suffix>"]), tag=tag) for tag in tags]
 
     def find_paradigm(self, lemma: str) -> str:
+        """Returns paradigm for given lemma or '' if lemma is not in vocabulary"""
         return self.vocab.get(lemma, "")
 
     def has_full_paradigm(self, lemma: str, k_tag: int) -> bool:
+        """Checks whether tags present in lemma form a full paradigm"""
         all_tags = []
         for tags in self.all_forms(lemma).values():
             all_tags.extend(tags)
@@ -121,6 +131,7 @@ class MorphDatabase:
         return not needed
 
     def matching_paradigms(self, lemma: str) -> List[str]:
+        """Returns all paradigms matching given lemma - TBD"""
         matching = []
         for paradigm, suffices in self.paradigms.items():
             if lemma.endswith(suffices["<suffix>"]):
@@ -128,6 +139,7 @@ class MorphDatabase:
         return matching
 
     def paradigm_roots(self) -> None:
+        """Assigns suffix to each paradigm in database"""
         for paradigm, suffices in self.paradigms.items():
             found = False
             for suffix, tags in suffices.items():
@@ -143,6 +155,7 @@ class MorphDatabase:
 
 
 def longest_fitting_suffix(paradigm: str, suffices: FORMS) -> str:
+    """Returns that longest one from suffixes which paradigm ends with"""
     longest = ""
     for suffix in suffices.keys():
         if paradigm.endswith(suffix) and len(suffix) > len(longest):
@@ -151,6 +164,7 @@ def longest_fitting_suffix(paradigm: str, suffices: FORMS) -> str:
 
 
 def translate_morph_db(par_file: str) -> PARADIGMS_TAIL_GROUPS:
+    """Transform data from paradigm file to final paradigm database"""
     translated = dict()
     paradigm_db, tails = read_paradigms(par_file)
     for paradigm, forms in paradigm_db.items():
@@ -164,21 +178,8 @@ def translate_morph_db(par_file: str) -> PARADIGMS_TAIL_GROUPS:
     return translated
 
 
-def suffix_database(full_db: MORPH_DATABASE) -> SUFFICES_DATABASE:
-    suffices = dict()
-    for lemma, forms in full_db.items():
-        for form, tags in forms.items():
-            compressed_form = compress_word(lemma.split("_")[0], form)
-            if compressed_form not in suffices:
-                suffices[compressed_form] = dict()
-            for tag, count in tags.items():
-                if tag not in suffices[compressed_form]:
-                    suffices[compressed_form][tag] = 0
-                suffices[compressed_form][tag] += count
-    return suffices
-
-
 def compress_word(lemma: str, word: str) -> str:
+    """Compress word form to format <pop from lemma>|<append to remainder>"""
     common = lemma
     while not word.startswith(common):
         common = common[:-1]
@@ -186,6 +187,7 @@ def compress_word(lemma: str, word: str) -> str:
 
 
 def vocabulary(dic_file: str) -> VOCABULARY:
+    """Creates vocabulary from data in dictionary file"""
     vocab = dict()
     d = open(dic_file, "r", encoding="windows-1250")
     for line in d:
@@ -199,6 +201,7 @@ def vocabulary(dic_file: str) -> VOCABULARY:
 
 
 def read_paradigms(par_file: str) -> Tuple[PARADIGMS_TAIL_GROUPS, TAILS]:
+    """Reads paradigms file to dictionaries which wil be merged to paradigm database"""
     # tails -> koncovky
     tails = dict()
     database = dict()
@@ -223,6 +226,7 @@ def read_paradigms(par_file: str) -> Tuple[PARADIGMS_TAIL_GROUPS, TAILS]:
 
 
 def correct_encoding(line: str) -> str:
+    """Replaces wrongly encodes characters from dictionary and paradigm files"""
     return line.replace("ą", "š").replace("ľ", "ž").replace("»", "ť").replace("®", "Ž").replace("©", "Š")
 
 
