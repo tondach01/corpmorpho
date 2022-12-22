63d09e61700f19e98364d900ffbf9d9c992526e8
xmetelk1 <xmetelk1@mail.muni.cz>
2022-12-22 14:47:47 +0100

    guessing paradigm from word

--- guesser.py
+++ guesser.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
-import morph_database
-from typing import List
+import morph_database as md
+import db_stats
+from typing import List, Dict
+from os import sep
 
 
-def fit_suffices(word: str, suffices_db: morph_database.SUFFICES_DATABASE) -> List[str]:
+def guess_paradigm(word: str, corpus: str, morph_db: md.MorphDatabase) -> Dict[str, int]:
+    """Guesses the probabilities of paradigms for given word, bigger matched suffixes are prioritized"""
+    score = dict()
+    for i in range(len(word)):
+        suffix = word[i:]
+        for paradigm, freq in db_stats.suffix_frequencies(corpus, morph_db, suffix).items():
+            if paradigm not in score.keys() or score[paradigm] < freq*len(suffix):
+                score[paradigm] = freq*len(suffix)
+    return score
+
+
+def fit_suffices(word: str, suffices_db: md.SUFFICES_DATABASE) -> List[str]:
     fitting = []
     for suffix in suffices_db.keys():
         if matches_suffix(word, suffix):
@@ -20,23 +33,10 @@ def lemma(word: str, suffix: str) -> str:
 
 
 def main():
-    import group_word_forms
-    import sentencepiece as sp
-
+    desam = f"C:{sep}Users{sep}ondra{sep}Desktop{sep}MUNI{sep}PB106{sep}data{sep}desam_model{sep}desam"
     word = "pochybovačem"
-    m = sp.SentencePieceProcessor()
-    m.load('sentencepiece\\m.model')
-    print(m.encode_as_pieces(word))
-    k = m.encode_as_pieces(word)[-1]
-
-    md = group_word_forms.read_corpora(open("..\\..\\PB106\\data\\desam_model\\desam", "r", encoding="utf-8"))
-    # md = morph_database.main()
-
-    sd = morph_database.suffix_database(md)
-    s = fit_suffices(word, sd)
-    for suffix in s:
-        if suffix.split("|")[1] in k:
-            print(f"{lemma(word, suffix)}: {sum(sd[suffix].values())}")
+    g = guess_paradigm(word, desam, md.MorphDatabase("current.dic", "current.par"))
+    pass
 
 
 if __name__ == "__main__":
