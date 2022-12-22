162229ecfdca4a2150782ca5007da30b1b478461
xmetelk1 <xmetelk1@mail.muni.cz>
2022-12-22 13:40:22 +0100

    frequencies with generating lemmas a filtering by suffix

--- db_stats.py
+++ db_stats.py
@@ -3,25 +3,32 @@ from typing import Dict
 from os import sep
 
 
-def paradigm_frequencies(corpus: str, morph_db: md.MorphDatabase) -> Dict[str, int]:
-    """Computes frequencies of words belonging to single paradigm in corpus"""
-    freqs = dict()
+def lemmas(corpus: str):
+    """Generates all lemmas in given corpus"""
     corp = open(corpus, encoding="utf-8")
     for line in corp:
         if len(line.split("\t")) != 3:
             continue
-        lemma = line.split("\t")[1]
-        if lemma not in morph_db.vocab.keys():
+        yield line.split("\t")[1]
+    corp.close()
+
+
+def paradigm_frequencies(corpus: str, morph_db: md.MorphDatabase, suffix: str = "") -> Dict[str, int]:
+    """Computes frequencies of lemmas belonging to single paradigm in corpus. If given suffix,
+    limits the search only to lemmas ending with it."""
+    freqs = dict()
+    for lemma in lemmas(corpus):
+        if not lemma.endswith(suffix) or lemma not in morph_db.vocab.keys():
             continue
         paradigm = morph_db.vocab[lemma]
         freqs[paradigm] = freqs.get(paradigm, 0) + 1
-    corp.close()
     return freqs
 
 
 def main():
     desam = f"C:{sep}Users{sep}ondra{sep}Desktop{sep}MUNI{sep}PB106{sep}data{sep}desam_model{sep}desam"
-    paradigm_frequencies(desam, md.MorphDatabase("current.dic", "current.par"))
+    p = paradigm_frequencies(desam, md.MorphDatabase("current.dic", "current.par"), "ek")
+    pass
 
 
 if __name__ == "__main__":
