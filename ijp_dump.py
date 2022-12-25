621aedb7b9926718de6b24fb16b7d42854d84ac2
xmetelk1 <xmetelk1@mail.muni.cz>
2022-12-25 18:26:17 +0100

    add dumping from Internetová Jazyková Příručka

---
+++ ijp_dump.py
@@ -0,0 +1,51 @@
+import pandas as pd
+
+
+CATEGORIES = {'jednotné číslo': 'nS', 'množné číslo': 'nP', '1. osoba': 'p1', '2. osoba': 'p2', '3. osoba': 'p3',
+              'rozkazovací způsob': 'mR', 'příčestí činné': 'mA', 'příčestí trpné': 'mN',
+              'přechodník přítomný, m.': 'mS', 'přechodník přítomný, ž. + s.': 'mS', 'přechodník minulý, m.': 'mD',
+              'přechodník minulý, ž. + s.': 'mD', '1. pád': 'c1', '2. pád': 'c2', '3. pád': 'c3', '4. pád': 'c4',
+              '5. pád': 'c5', '6. pád': 'c6', '7. pád': 'c7'}
+
+
+def table_dump(word: str):
+    import urllib.parse as p
+    df = pd.read_html(f"https://prirucka.ujc.cas.cz/?slovo={p.quote(word)}", encoding="utf-8")[0]
+    for i, row in enumerate(df.values):
+        for j, val in enumerate(row):
+            if str(val)[-1].isdigit():
+                df[j][i] = val[:-1]
+            elif str(val) in CATEGORIES.keys():
+                df[j][i] = CATEGORIES[str(val)]
+    return df
+
+
+def df2dict(df: pd.DataFrame):
+    res = dict()
+    header = None
+    if str(df[0][0]) == "nan":
+        header = df.values[0]
+    for i in range(len(df.values)):
+        if i == 0 and header is not None:
+            continue
+        for j in range(len(df.values[0])):
+            if j == 0:
+                continue
+            if ',' in str(df[j][i]):
+                val = str(df[j][i]).split(', ')
+            else:
+                val = [str(df[j][i])]
+            if header is not None:
+                res[header[j]] = res.get(header[j], dict())
+                res[header[j]][df[0][i]] = val
+            else:
+                res[df[0][i]] = val
+    return res
+
+
+def main():
+    print(df2dict(table_dump("samohonka")))
+
+
+if __name__ == "__main__":
+    main()
