ca76fa32c952c6b74ade514c934f6bcf97430271
xmetelk1 <xmetelk1@mail.muni.cz>
2022-12-29 17:53:02 +0100

    method with segmenters - sentencespiece

--- compare_segmenters.py
+++ compare_segmenters.py
@@ -1,11 +1,12 @@
 import morph_database as md
 import guesser as g
 from typing import Tuple
+from os import sep
 
 
 def baseline_guess(test_vocab: str, corpus: str, morph_db: md.MorphDatabase,
                    top_n: int = 1, debug: bool = False) -> Tuple[int, int]:
-    """Tries to guess paradigm for each lemma in test_vocab and return its success rate (all, correct)"""
+    """Tries to guess paradigm for each lemma in test_vocab and returns its success rate (all, correct)"""
     all_pars, correct = 0, 0
     with open(test_vocab, encoding="windows-1250") as test:
         for line in test:
@@ -24,12 +25,40 @@ def baseline_guess(test_vocab: str, corpus: str, morph_db: md.MorphDatabase,
     return all_pars, correct
 
 
+def segmented_guess(test_vocab: str, corpus: str, morph_db: md.MorphDatabase, segmenter: str,
+                    top_n: int = 1, debug: bool = False) -> Tuple[int, int]:
+    """Tries to guess paradigm for each lemma in test_vocab if given its segmentation
+    and returns its success rate (all, correct)"""
+    segment = (lambda x: list(x[i:] for i in range(len(x))))
+    if segmenter == "sentencepiece":
+        import sentencepiece as sp
+        m = sp.SentencePieceProcessor()
+        m.load(f"sentencepiece{sep}m.model")
+        segment = m.encode_as_pieces
+    all_pars, correct = 0, 0
+    with open(test_vocab, encoding="windows-1250") as test:
+        for line in test:
+            if debug:
+                print(line.strip())
+            lem_par = line.strip().split(":")
+            segments = segment(lem_par[0])
+            score = g.guess_paradigm_from_lemma_seg(lem_par[0], corpus, morph_db, segments)
+            paradigms = list(sorted(score, key=(lambda x: score[x]), reverse=True))
+            if debug:
+                print("\t" + ", ".join(paradigms))
+            if len(paradigms) < top_n and lem_par[1] in paradigms:
+                correct += 1
+            elif lem_par[1] in paradigms[:top_n]:
+                correct += 1
+            all_pars += 1
+    return all_pars, correct
+
+
 def main():
-    from os import sep
     from time import time
     start = time()
     train, test = md.MorphDatabase("current.dic", "current.par").split_vocabulary()
-    a, c = baseline_guess(test, f"desam{sep}desam", md.MorphDatabase(train, "current.par"), debug=True)
+    a, c = segmented_guess(test, f"desam{sep}desam", md.MorphDatabase(train, "current.par"), "sentencepiece", debug=True)
     print(f"finished in {round(time() - start)}s, {c} correct out of {a}")
 
 
