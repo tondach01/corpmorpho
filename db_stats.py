45cb2c7f1a74b13f759119d42feb8dd518f03f55
tondach01 <ondra.metelka@seznam.cz>
2023-02-22 17:22:21 +0100

    remove main

--- db_stats.py
+++ db_stats.py
@@ -55,32 +55,3 @@ def print_scores(scores: Dict[str, int]) -> None:
     for paradigm in sorted(scores, key=(lambda x: scores[x]), reverse=True):
         print(f"{paradigm}: {scores[paradigm]}")
     print()
-
-
-def main():
-    '''
-    from time import time
-    from os import sep
-    desam = f"desam{sep}desam"
-    morph_db = md.MorphDatabase("data/current.dic", "data/current.par")
-    word = "kámen"
-    segments = [word[- i:] for i in range(1, len(word) + 1)]
-    df = lemmas_to_dataframe(desam, morph_db)
-    start = time()
-    # p = suffix_frequencies(desam, md.MorphDatabase("current.dic", "current.par"), "čka")
-    # p = paradigm_frequencies(desam, md.MorphDatabase("current.dic", "current.par"))
-    # print_score(p)
-    pandas_lemma_scores(segments, df)
-    checkpoint = time()
-    print(f"pandas version finished in {round(checkpoint - start, 3)}s")
-    lemma_scores(desam, morph_db, segments)
-    print(f"classic version finished in {round(time() - checkpoint, 3)}s")
-    '''
-    from os import sep
-    morph_db = md.MorphDatabase("data/current.dic", "data/current.par")
-    k = freqlist_to_dataframe(f"data{sep}mu_theses_czech.freqlist", morph_db, limit=100)
-    pass
-
-
-if __name__ == "__main__":
-    main()
