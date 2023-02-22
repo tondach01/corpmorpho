844952452c5acf58d48afba073f71c24beb6daf3
tondach01 <ondra.metelka@seznam.cz>
2023-02-22 09:38:04 +0100

    add initial CLI interface

--- guesser.py
+++ guesser.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 import db_stats
 from typing import Dict, List
 
@@ -15,3 +16,31 @@ def guess_paradigm_from_lemma(segments: List[str], frame) -> Dict[str, int]:
     """Guesses the probabilities of paradigms for given lemma and its segmentation,
     bigger matched suffixes are prioritized."""
     return guess_paradigm(segments, None, frame, only_lemmas=True)
+
+
+def main(infile: str = None, lemmatized: bool = False, segment: str = ""):
+    # TODO
+    import sys
+    if infile is None:
+        source = sys.stdin
+    elif not os.path.exists(infile):
+        print(f"File {infile} not found, using stdin", file=sys.stderr)
+        source = sys.stdin
+    else:
+        source = open(infile, "r")
+    scores = guess_paradigm
+    print(infile, lemmatized)
+    pass
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="Guesses the paradigm of given word")
+    parser.add_argument("-l", "--lemma", action="store_true", help="word is given in its base form", default=False)
+    parser.add_argument("-f", "--infile", help="file from which take the words for guessing (one per line), "
+                                               "otherwise stdin is used")
+    parser.add_argument("-s", "--use-segmenter", choices=["sentencepiece", "morfessor"], default="",
+                        help="use segmentation for words (if not specified, not using any)")
+    args = parser.parse_args()
+
+    main(args.infile, args.lemma, args.use_segmenter)
