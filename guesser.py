6e1738214dd4d6487a993568809154b7b4593480
tondach01 <ondra.metelka@seznam.cz>
2023-02-22 09:49:29 +0100

    move get_segment_method to guesser

--- guesser.py
+++ guesser.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 import os
-import db_stats
+import db_stats as dbs
+import morph_database as md
 from typing import Dict, List
 
 
 def guess_paradigm(segments: List[str], morph_db, frame, only_lemmas: bool = False) -> Dict[str, int]:
     """Guesses the probabilities of paradigms for given word and its sub-word segmentation,
     bigger matched suffixes are prioritized. Note: very slow for non-lemmatized word"""
-    search_func = db_stats.pandas_lemma_scores
+    search_func = dbs.pandas_lemma_scores
     # TODO function for non-lemmatized
     return search_func(["".join(segments[-i:]) for i in range(1, len(segments) + 1)], frame)
 
@@ -16,21 +17,6 @@ def guess_paradigm_from_lemma(segments: List[str], frame) -> Dict[str, int]:
     """Guesses the probabilities of paradigms for given lemma and its segmentation,
     bigger matched suffixes are prioritized."""
     return guess_paradigm(segments, None, frame, only_lemmas=True)
-
-
-def main(infile: str = None, lemmatized: bool = False, segment: str = ""):
-    # TODO
-    import sys
-    if infile is None:
-        source = sys.stdin
-    elif not os.path.exists(infile):
-        print(f"File {infile} not found, using stdin", file=sys.stderr)
-        source = sys.stdin
-    else:
-        source = open(infile, "r")
-    scores = guess_paradigm
-    print(infile, lemmatized)
-    pass
 
 
 def get_segment_method(segmenter: str):
@@ -61,6 +47,21 @@ def get_segment_method(segmenter: str):
     return baseline
 
 
+def main(infile: str = None, lemmatized: bool = False, segment: str = ""):
+    # TODO
+    import sys
+    if infile is None:
+        source = sys.stdin
+    elif not os.path.exists(infile):
+        print(f"File {infile} not found, using stdin", file=sys.stderr)
+        source = sys.stdin
+    else:
+        source = open(infile, "r")
+    morph_db = md.MorphDatabase(f"..{os.sep}data{os.sep}current.dic", f"..{os.sep}data{os.sep}current.par")
+    scores = guess_paradigm(get_segment_method(segment), morph_db,
+                            dbs.lemmas_to_dataframe(f"..{os.sep}desam{os.sep}desam", morph_db), lemmatized)
+
+
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="Guesses the paradigm of given word")
@@ -71,4 +72,8 @@ if __name__ == "__main__":
                         help="use segmentation for words (if not specified, not using any)")
     args = parser.parse_args()
 
+    if not os.path.exists(f".{os.sep}temp"):
+        os.mkdir(f".{os.sep}temp")
+    os.chdir(f".{os.sep}temp")
     main(args.infile, args.lemma, args.use_segmenter)
+    os.chdir("..")
