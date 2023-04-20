149e0db50c2a6c920aaef7602b9fc8e447f6b656
tondach01 <ondra.metelka@seznam.cz>
2023-04-20 12:27:26 +0200

    move out plotting

--- eval_logs.py
+++ eval_logs.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-from typing import Tuple, List, Dict, Any
+from typing import Tuple, List
 from os import sep, listdir
-# import matplotlib.pyplot as plt
 
 
 def evaluate(log_file: str, top_n: int = 5) -> Tuple[List[int], int, int]:
@@ -39,47 +38,6 @@ def eval_all_logs() -> None:
         full_eval(f"logs{sep}{log_file}")
 
 
-def plot_results(x_axis: List[Any], data: Dict[str, List[Any]], x_label: str = "", y_label: str = "") -> None:
-    for label, values in data.items():
-        plt.plot(x_axis, values, label=label)
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    plt.legend()
-    plt.show()
-
-
-def sentencepiece_plot(top_n: int = 1):
-    data = {"bpe": [0 for _ in range(20)], "unigram": [0 for _ in range(20)]}
-    for (tool, model, vocab_size), entry in load_results().items():
-        if tool != "sentencepiece":
-            continue
-        data[model][(vocab_size // 2000) - 1] = entry["precisions"][top_n - 1]
-    plot_results(list(range(2000, 40001, 2000)), data, "Vocabulary size", f"Correct in {top_n}")
-
-
-def newest_res_file() -> str:
-    from os import path
-    return "results" + sep + max(listdir("results"), key=(lambda x: path.getctime(f"results{sep}{x}")))
-
-
-def load_results() -> Dict[Tuple[str, str, int], Any]:
-    loaded = dict()
-    with open(newest_res_file(), encoding="utf-16le") as res:
-        for line in res:
-            if "log" in line:
-                names = line.strip().split("_")
-                tool, model, vocab_size = names[1].rstrip(":"), "" if len(names) < 3 else names[2].rstrip(":"), \
-                    0 if len(names) < 4 else int(names[3].rstrip(":"))
-                loaded[(tool, model, vocab_size)] = dict(test_size=0, average_guesses=0.0, precisions=[])
-            elif line.startswith("top"):
-                data = line.split(" ")
-                entry = loaded[(tool, model, vocab_size)]
-                entry["precisions"].append(int(data[2].rstrip(",").split("/")[0]))
-                entry["test_size"] = int(data[2].rstrip(",").split("/")[1])
-                entry["average_guesses"] = float(data[3])
-    return loaded
-
-
 def main():
     from time import time
     start = time()
@@ -89,4 +47,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    # sentencepiece_plot(4)
