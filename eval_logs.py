ac679cbec8f2bfd6534e0593c6d9a854e8313dba
tondach01 <ondra.metelka@seznam.cz>
2023-05-03 17:48:46 +0200

    add full log evaluation

--- eval_logs.py
+++ eval_logs.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 from typing import Tuple, List
-from os import sep, listdir
+from os import listdir
 from sys import stdout
 import morph_database as md
 
 
-def evaluate(log_file: str, top_n: int = 1) -> Tuple[List[int], int, int]:
+def top_n_check(log_file: str, top_n: int = 1) -> Tuple[List[int], int, int]:
     """Reads the given log file and evaluates its precision in 1 to <top_n> guesses.
     Returns (correct, all, total guesses)"""
     correct = [0 for _ in range(top_n)]
@@ -28,22 +28,40 @@ def evaluate(log_file: str, top_n: int = 1) -> Tuple[List[int], int, int]:
     return correct, entries, guess_count
 
 
-def full_eval(log_file: str, top_n: int = 1, outfile=stdout) -> None:
-    correct, entries, guess_count = evaluate(log_file, top_n)
-    for i, i_correct in enumerate(correct):
-        print(f"top {i+1}: {i_correct}/{entries}, {round(guess_count/entries, 3)} guesses in average", file=outfile)
+def full_eval(fltr: str = "", top_n: int = 1, threshold: int = 5, debug: bool = False) -> None:
+    morph_db = md.MorphDatabase("data/current.dic", "data/current.par")
+    outfile = stdout
+    if not debug:
+        import datetime
+        outfile = open(f"results/{datetime.date.today()}_all.txt", "w", encoding="utf-8")
+    for log_file in listdir("logs"):
+        if fltr not in log_file:
+            continue
+        correct, entries, guess_count = top_n_check(f"logs/{log_file}", top_n)
+        print(f"{log_file}: {entries} examples, {round(guess_count, 3)} guesses at average", file=outfile)
+        for i in range(len(correct)):
+            print(f"\tsame_paradigms_top_{i + 1}: {correct[i]} (precision {round(correct[i] / entries, 3)})",
+                  file=outfile)
+        for crit in ["same_affixes", "common_forms", "common_tags", "same_lemma"]:
+            correct, entries, guess_count = md_check(f"logs/{log_file}", crit, morph_db, threshold)
+            print(f"\t{crit}{f'_{threshold}' if crit == 'common_forms' else ''}: {correct} "
+                  f"(precision {round(correct / entries, 3)})", file=outfile)
+    if not debug:
+        outfile.close()
 
 
 def classic_eval(fltr: str = "", top_n: int = 1, debug: bool = False) -> None:
     outfile = stdout
     if not debug:
         import datetime
-        outfile = open(f"results/{datetime.date.today()}.txt", "w", encoding="utf-8")
+        outfile = open(f"results/{datetime.date.today()}_top_n.txt", "w", encoding="utf-8")
     for log_file in listdir("logs"):
         if fltr not in log_file:
             continue
-        print(f"{log_file}:", file=outfile)
-        full_eval(f"logs{sep}{log_file}", top_n, outfile)
+        correct, entries, guess_count = top_n_check(log_file, top_n)
+        print(f"{log_file}: {entries} examples, {round(guess_count, 3)} guesses at average", file=outfile)
+        for i in range(len(correct)):
+            print(f"\ttop_{i + 1}: {correct[i]} (precision {round(correct[i] / entries, 3)})", file=outfile)
     if not debug:
         outfile.close()
 
@@ -89,9 +107,9 @@ def main():
     from time import time
     import argparse
     parser = argparse.ArgumentParser(description="Evaluate logs in ./logs/ directory")
-    parser.add_argument("-c", "--criterion", choices=["same_paradigms", "same_affixes", "common_forms", "common_tags",
-                                                      "same_lemma"],
-                        help="how to evaluate", default="same_paradigms")
+    parser.add_argument("-c", "--criterion", choices=["all", "same_paradigms", "same_affixes", "common_forms",
+                                                      "common_tags", "same_lemma"],
+                        help="how to evaluate", default="all")
     parser.add_argument("-f", "--filter", help="only log names containing this string will be evaluated", default="")
     parser.add_argument("-t", "--threshold", type=int, help="threshold for common_forms", default=5)
     parser.add_argument("-d", "--debug", action="store_true", help="print to standard output", default=False)
@@ -100,6 +118,8 @@ def main():
     start = time()
     if args.criterion == "same_paradigms":
         classic_eval(args.filter, args.top_n, args.debug)
+    elif args.criterion == "all":
+        full_eval(args.filter, args.top_n, args.threshold, args.debug)
     else:
         md_eval(args.criterion, args.filter, args.threshold, args.debug)
     print(f"finished in {round(time() - start)}s")
